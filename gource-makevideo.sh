2422cfb0f24ec10864e060719995485a0b84a457
m1kc (Max Musatov) <m1kc@yandex.ru>
2013-08-20 17:07:14 +0400

    Higher!

--- gource-makevideo.sh
+++ gource-makevideo.sh
@@ -8,14 +8,14 @@ if [ $1 == "--xvid" ]; then
 		ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - \
 		-vcodec libxvid \
 		-mbd rd -flags +mv4+aic -trellis 2 -cmp 2 -subcmp 2 -g 300 \
-		-vb 2000k \
+		-vb 9000k \
 		gource.avi
 elif [ $1 == "--mpeg4" ]; then
 	gource --highlight-users -s 0.25 -a 0.1 -1280x720 --output-ppm-stream - | \
 		ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - \
 		-vcodec mpeg4 \
 		-mbd rd -flags +mv4+aic -trellis 2 -cmp 2 -subcmp 2 -g 300 \
-		-vb 2000k \
+		-vb 9000k \
 		gource.avi
 else
 	gource --highlight-users -s 0.25 -a 0.1 -1280x720 --output-ppm-stream - | \
