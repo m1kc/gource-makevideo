9a5230aecfd0e803e6c759959cede377a089e82f
m1kc (Max Musatov) <m1kc@yandex.ru>
2013-08-20 16:28:07 +0400

    Xvid support.

--- gource-makevideo.sh
+++ gource-makevideo.sh
@@ -3,7 +3,16 @@ echo "gource-makevideo 1.0"
 # devel:
 #        -c 4
 #        -s 0.1
-gource --highlight-users -s 0.25 -a 0.1 -1280x720 --output-ppm-stream - | \
-	ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - \
-	-x264opts "subq=6:partitions=all:8x8dct:me=umh:frameref=5:bframes=3:b_pyramid=normal:weight_b:crf=19" \
-	gource.mp4
+if [ $1 == "--xvid" ]; then
+	gource --highlight-users -s 0.25 -a 0.1 -1280x720 --output-ppm-stream - | \
+		ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - \
+		-vcodec libxvid \
+		-mbd rd -flags +mv4+aic -trellis 2 -cmp 2 -subcmp 2 -g 300 \
+		-vb 500k \
+		gource.avi
+else
+	gource --highlight-users -s 0.25 -a 0.1 -1280x720 --output-ppm-stream - | \
+		ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - \
+		-x264opts "subq=6:partitions=all:8x8dct:me=umh:frameref=5:bframes=3:b_pyramid=normal:weight_b:crf=19" \
+		gource.mp4
+fi
