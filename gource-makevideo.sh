3522f81cb649b562bd1610185cddd554e894f925
m1kc (Max Musatov) <m1kc@yandex.ru>
2014-01-28 08:14:00 +0400

    Minor fix.

--- gource-makevideo.sh
+++ gource-makevideo.sh
@@ -3,14 +3,14 @@ echo "gource-makevideo 1.0"
 # devel:
 #        -c 4
 #        -s 0.1
-if [ $1 == "--xvid" ]; then
+if [ "$1" == "--xvid" ]; then
 	gource --highlight-users -s 0.25 -a 0.1 -1280x720 --output-ppm-stream - | \
 		ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - \
 		-vcodec libxvid \
 		-mbd rd -flags +mv4+aic -trellis 2 -cmp 2 -subcmp 2 -g 300 \
 		-vb 9000k \
 		gource.avi
-elif [ $1 == "--mpeg4" ]; then
+elif [ "$1" == "--mpeg4" ]; then
 	gource --highlight-users -s 0.25 -a 0.1 -1280x720 --output-ppm-stream - | \
 		ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - \
 		-vcodec mpeg4 \
